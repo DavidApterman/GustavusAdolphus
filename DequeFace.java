a59c4d82a7abe7354ce0db3e863895dcd0f2a5a7
Jenny Han <jhan6@stuy.edu>
2017-03-31 10:02:43 -0400

    added interace

---
+++ DequeFace.java
@@ -0,0 +1,9 @@
+public interface DequeFace <J> {
+    /* public void addFirst (J element);
+    public void addLast (J element);
+    public J pollFirst ();
+    public J pollLast ();
+    public J peekFirst ();
+    public J peekLast ();*/
+}
+
