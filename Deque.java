bfc562db413c265f9191d61dbb2fb7917a1b9da7
Jenny Han <jhan6@stuy.edu>
2017-04-03 09:57:31 -0400

    added comments

--- Deque.java
+++ Deque.java
@@ -2,11 +2,13 @@ public class Deque <J> implements DequeFace <J> {
     
     private DLLNode <J> _front, _end;
 
+    //sets the front and end pointer to null
     public Deque () {
 	_front = null;
 	_end = null;
     }
 
+    //if the deque is empty, sets the front and end pointer to a new node with cargo enQVal; if not, sets the node directly preceding the front node to a new node with cargo enQVal and shifts the front pointer to the new node
     public void addFirst (J enQVal) {
 	if (isEmpty())
 	    _front = _end = new DLLNode <J> (enQVal,null, null);
@@ -16,6 +18,7 @@ public class Deque <J> implements DequeFace <J> {
 	}
     }
 
+    //if the deque is empty, sets the front and end pointer to a new node with cargo enQVal; if not, set the node directly proceeding the end node to a new node with cargo enQVal and shifts the end pointer to the new node
     public void addLast (J enQVal) {
 	if (isEmpty())
 	    _front = _end = new DLLNode <J> (enQVal, null, null);
@@ -24,7 +27,8 @@ public class Deque <J> implements DequeFace <J> {
 	    _end = _end.getNext(); 
 	}
     } 
-    
+
+    //if the deque is empty, then returns null because there is no first node to remove; if not, sets the front pointer to the node directly proceeding it, gets the cargo of the original front node, and sets it to null
     public J removeFirst () {
 	if ( isEmpty() ) {
 	    System.out.println("nothing to remove");
@@ -36,6 +40,7 @@ public class Deque <J> implements DequeFace <J> {
 		}
     }
 
+    //if the deque is empty, then returns null because there is no last node to remove; if not, sets the end pointer to the node directly preceding it, gets the cargo of the original end node, and sets it to null
       public J removeLast () {
 	if ( isEmpty() ) {
 	    System.out.println("nothing to remove");
@@ -47,18 +52,22 @@ public class Deque <J> implements DequeFace <J> {
 		}
     }
 
+    //returns the cargo of the node that first is pointing to 
     public J pollFirst() {
 	return _front.getCargo();
     }
 
+    //returns the cargo of the node that end is pointing to 
     public J pollLast() {
 	return _end.getCargo();
     }
-    
+
+    //returns true if deque is empty, false if deque is not
     public boolean isEmpty () {
 	return _front == null;
     }
 
+    //loops through the deque until it reaches null, which is when the deque ends and adds each node to the return String
     public String toString () {
 	String ret = "FRONT -> ";
 	DLLNode <J> temp = _front;
